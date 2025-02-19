da31501bd8b04c9e2db53b8e2aac471218f3b6b8
Karthick Balaji <karthickthug07@gmail.com>
2025-02-19 08:58:37 +0530

    Add files via upload

---
+++ lab 2 exp 1.R
@@ -0,0 +1,13 @@
+sieve <- function(n) {
+  primes <- rep(TRUE, n)
+  primes[1] <- FALSE
+  for (i in 2:sqrt(n)) {
+    if (primes[i]) {
+      primes[seq(i^2, n, i)] <- FALSE
+    }
+  }
+  which(primes)
+}
+
+n <- as.integer(readline(prompt="Enter a number: "))
+cat("Prime numbers up to", n, ":", sieve(n), "\n")
